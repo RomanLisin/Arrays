92d590b3bfac0793aae0bcd664a68c0d6d581758
Lisin_sw23@top-academy.ru <Lisin_sw23@top-academy.ru>
2024-02-06 22:56:36 +1000

    Add project files.

---
+++ Shift/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+#define K k%n //multiplicity
+
+void main() {
+	int n;
+	setlocale(LC_ALL, "");
+	const int N = 10;
+	int arr[N];
+	int shift;
+	bool directShift = 0;
+
+	cout << "Input number for shift: "; cin >> shift;
+	cout << "If you want to move to the right, enter any number other than 0, enter 0: : "; cin >> directShift;
+
+	// F(n) = F(n - 1) + F(n - 2)  Fibonacci formula
+		for (int i = 0; i < N; i++) {
+
+			arr[i] = (i > 1 ? arr[i - 1] + arr[i - 2] : i); // Fibonacci sequence
+
+		}
+		cout << endl << endl;
+
+	for (int k = 0; k <= shift; k++) {
+		for (int i = 0; i < N; i++) {
+			if (directShift) cout<<(i >= K ? arr[i - K] : arr[i + N - K]) << "\t";  // right shift
+			else  cout << (i < (N - K) ? arr[i + K] : arr[i - N + K]) << "\t";		// left shift
+
+		}
+		if (k == 0)cout << endl; cout << endl;
+
+	}
+	cout << endl << endl;
+
+}
